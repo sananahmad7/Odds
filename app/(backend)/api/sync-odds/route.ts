05dff29c4399505d43661783bdf789d5331ebe1c
Sanan Ahmad <sananahmad909@gmail.com>
2025-12-04 16:19:44 +0500

    logos addded in odds

--- app/(backend)/api/sync-odds/route.ts
+++ app/(backend)/api/sync-odds/route.ts
@@ -1,7 +1,8 @@
 // app/api/odds-sync/route.ts
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { handleSyncOdds } from "@/utils/sync-odds";
+import { prisma } from "@/lib/prisma";
 
 // -----------------------------
 // Route
@@ -11,4 +12,31 @@ export async function POST() {
   const result = await handleSyncOdds({ prune: true });
   const status = result.success ? 200 : 500;
   return NextResponse.json(result, { status });
+}
+
+export async function PUT(req: NextRequest) {
+  // 1. Get id from query string
+  const eventId = req.nextUrl.searchParams.get("id");
+  const { image } = await req.json();
+
+  if (!eventId) {
+    return NextResponse.json(
+      { error: "Missing event id in query (?id=...)" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const updatedEvent = await prisma.oddsEvent.update({
+      data: { image },
+      where: { id: eventId },
+    });
+
+    return NextResponse.json(updatedEvent);
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: `Error in update hero image, error: ${err}` },
+      { status: 500 }
+    );
+  }
 }
